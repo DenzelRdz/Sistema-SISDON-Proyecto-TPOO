0757ada05ac4baef32fdb7e1c8fca73d6a86b2b4
Denzel <danielrdz.ddro@gmail.com>
2024-05-19 10:37:43 -0600

    Nuevo diseño del login y codigo de inicio de sesion

--- Forms/Login.cs
+++ Forms/Login.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Data.OleDb;
+
 
 namespace Sistema_SISDON_Proyecto_TPOO.Forms
 {
@@ -18,7 +20,8 @@ namespace Sistema_SISDON_Proyecto_TPOO.Forms
             InitializeComponent();
         }
 
-        SqlConnection conexion = new SqlConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Xiomara\Source\Repos\Sistema-SISDON-Proyecto-TPOO\TortilleriaDonTitoDB.accdb"); 
+        OleDbConnection conexion = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\TortilleriaDonTitoDB.accdb");
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -27,15 +30,77 @@ namespace Sistema_SISDON_Proyecto_TPOO.Forms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Dev_Menu dev_Menu = new Dev_Menu();
-            this.Hide();
-            dev_Menu.Show();
+            string nombre = txtUsuario.Text;
+            string contraseña = txtClave.Text;
+
+            try
+            {
+                conexion.Open();
+
+                string query = "SELECT * FROM empleado WHERE nombre = @nombre AND contrasena = @contrasena";
+                OleDbCommand cmd = new OleDbCommand(query, conexion);
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@contrasena", contraseña);
+
+                OleDbDataAdapter sda = new OleDbDataAdapter(cmd);
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                if (dtable.Rows.Count > 0)
+                {
+                    Dev_Menu dev_Menu = new Dev_Menu();
+                    dev_Menu.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Credenciales incorrectas");
+                    txtUsuario.Clear();
+                    txtClave.Clear();
+                    txtUsuario.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conexion.Close();
+            }
         }
+
+
 
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             txtUsuario.Clear();
             txtClave.Clear();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtClave_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
